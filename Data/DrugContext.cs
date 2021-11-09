674aba1972da199cb4121bf089ad4eb2cd843537
devyg <devyg@YGORPC>
2021-11-09 01:07:09 -0300

    Add project files.

---
+++ Data/DrugContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PloomesPraticalTest.Models;
+
+namespace PloomesPraticalTest.Data
+{
+    public class DrugContext : DbContext
+    {
+        public DrugContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private readonly IConfiguration _configuration;
+        public DbSet<Drug> Drugs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("dbPloomes"));
+        }
+    }
+}
---
+++ Models/Drug.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PloomesPraticalTest.Models
+{
+    public class Drug
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public Boolean IsGeneric { get; set; }
+    }
+}
